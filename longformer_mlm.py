fix attribute masking and add zero shot recommendation evaluation

longformer_mlm.py
@@ -7,9 +7,13 @@ import torch
 from pytorch_lightning import seed_everything, Trainer
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning.loggers.wandb import WandbLogger
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 from transformers import LongformerForMaskedLM
 from wonderwords import RandomWord
 
+from collator import EvalDataCollatorWithPadding
+from dataloader import RecformerEvalDataset
 from recformer import RecformerTokenizer
 from recmlm import RecMLMConfig, RecMLMDataModule, RecMLM
 from utils import parse_mlm_args, load_data
@@ -29,7 +33,11 @@ def load_config_tokenizer_model(args, item2id):
     config.attention_window = [64] * 12
     config.max_token_num = 1024
     config.item_num = len(item2id)
+
+    config.session_reduce_method = args.session_reduce_method
+    config.pooler_type = args.pooler_type
     config.global_attention_type = args.global_attention_type
+    config.original_embedding = args.original_embedding
 
     tokenizer = RecformerTokenizer.from_pretrained(args.model_name_or_path, config=config)
     model = LongformerForMaskedLM.from_pretrained(args.model_name_or_path, config=config)
@@ -40,6 +48,7 @@ def load_config_tokenizer_model(args, item2id):
 def main(args: argparse.Namespace):
     torch.set_float32_matmul_precision("medium")
     seed_everything(42)
+    args.device = args.accelerator
 
     train, val, test, item_meta_dict, item2id, id2item = load_data(args)
     config, tokenizer, model = load_config_tokenizer_model(args, item2id)
@@ -57,7 +66,29 @@ def main(args: argparse.Namespace):
                                   id2item=id2item, item_meta_dict=item_meta_dict, batch_size=args.batch_size,
                                   num_workers=args.dataloader_num_workers)
 
-    module = RecMLM(model=model, learning_rate=args.learning_rate)
+    doc_tuples = [
+        _par_tokenize_doc(doc, tokenizer) for doc in
+        tqdm(item_meta_dict.items(), ncols=100, desc=f"[Tokenize] {path_corpus}")
+    ]
+    tokenized_items = {
+        item2id[item_id]: [input_ids, token_type_ids, attr_type_ids]
+        for item_id, input_ids, token_type_ids, attr_type_ids in doc_tuples
+    }
+    eval_data_collator = EvalDataCollatorWithPadding(tokenizer, tokenized_items)
+    val_data = RecformerEvalDataset(train, val, test, mode="val", collator=eval_data_collator)
+    test_data = RecformerEvalDataset(train, val, test, mode="test", collator=eval_data_collator)
+    rec_val_loader = DataLoader(
+        val_data, batch_size=args.batch_size * args.eval_test_batch_size_multiplier, collate_fn=val_data.collate_fn,
+        num_workers=args.dataloader_num_workers
+    )
+    rec_test_loader = DataLoader(
+        test_data, batch_size=args.batch_size * args.eval_test_batch_size_multiplier, collate_fn=test_data.collate_fn,
+        num_workers=args.dataloader_num_workers
+    )
+
+    module = RecMLM(args=args, config=config, model=model, tokenizer=tokenizer, learning_rate=args.learning_rate,
+                    tokenized_items=tokenized_items,
+                    rec_val_dataloader=rec_val_loader, rec_test_dataloader=rec_test_loader)
 
     # logger
     loggers = [
@@ -67,6 +98,7 @@ def main(args: argparse.Namespace):
         wandb_logger = WandbLogger(
             project="RecMLM",
             entity="gen-rec",
+            group=path_corpus.name,
             name=server_random_word_and_date,
         )
         loggers.append(wandb_logger)
@@ -83,8 +115,7 @@ def main(args: argparse.Namespace):
             save_top_k=1,
             filename="{epoch}-{val_loss:.2f}",
         ),
-        # pl.callbacks.RichModelSummary(max_depth=2),
-        # pl.callbacks.RichProgressBar(),
+
         pl.callbacks.EarlyStopping(
             monitor="val_loss",
             mode="min",
@@ -110,6 +141,7 @@ def main(args: argparse.Namespace):
     trainer.fit(module, datamodule=datamodule)
     trainer.test(ckpt_path="best", datamodule=datamodule)
     trainer.save_checkpoint(path_output / Path("model.ckpt"))
+    config.save_pretrained(path_output)
 
 
 if __name__ == "__main__":

recmlm/dataset.py
@@ -26,32 +26,14 @@ class RecMLMDataModule(LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-        if os.path.exists("valid_dataset.pt") and os.path.exists("test_dataset.pt"):
-            print(f"Loading Train Dataset from train_dataset.pt")
-            self.valid_dataset = torch.load("valid_dataset.pt")
-            self.test_dataset = torch.load("test_dataset.pt")
+        self.train_history, self.valid_history, self.test_history = self.get_history()
 
-        else:
-            _, self.valid_dataset, self.test_dataset = self.get_dataset()
-            torch.save(self.valid_dataset, "valid_dataset.pt")
-            torch.save(self.test_dataset, "test_dataset.pt")
-
-    def get_dataset(self, train=False):
+    def get_history(self):
         train_histories = list(list(self.user2train.values())[:-1])
         valid_histories = list(list(self.user2train.values()))
         test_histories = [self.user2train[user] + self.user2val[user] for user in self.user2val.keys()]
 
-        if train:
-            train_dataset = self.tokenize_dataset(train_histories)
-            print(f"Train_histories: {len(train_dataset)}")
-        else:
-            train_dataset = None
-        valid_dataset = self.tokenize_dataset(valid_histories)
-        print(f"Valid_histories: {len(valid_dataset)}")
-        test_dataset = self.tokenize_dataset(test_histories)
-        print(f"Test_histories: {len(test_dataset)}")
-
-        return train_dataset, valid_dataset, test_dataset
+        return train_histories, valid_histories, test_histories
 
     def tokenize_dataset(self, histories):
 
@@ -64,15 +46,15 @@ class RecMLMDataModule(LightningDataModule):
             masked_input_ids = []
             for idx, (input_id, token_id) in enumerate(
                     zip(tokenized_history['input_ids'], tokenized_history['token_type_ids'])):
-                is_mask = False
                 if token_id == 0:  # bos token
+                    is_mask = False
                     masked_input_ids.append(input_id)
                 elif token_id == 1:  # item token
                     is_mask = False
                     masked_input_ids.append(input_id)
                 elif token_id == 2 and tokenized_history['token_type_ids'][idx - 1] == 1:
                     # mask with mlm_ratio
-                    is_mask = True if torch.rand(1) < self.mlm_ratio else 0
+                    is_mask = True if torch.rand(1) < self.mlm_ratio else False
                     masked_input_ids.append(self.tokenizer.mask_token_id if is_mask else input_id)
                 elif token_id == 2:
                     masked_input_ids.append(self.tokenizer.mask_token_id if is_mask else input_id)
@@ -98,8 +80,9 @@ class RecMLMDataModule(LightningDataModule):
         )
 
     def val_dataloader(self):
+        valid_dataset = self.tokenize_dataset(self.valid_history)
         return torch.utils.data.DataLoader(
-            RecMLMDataset(self.valid_dataset),
+            RecMLMDataset(valid_dataset),
             batch_size=self.batch_size,
             shuffle=False,
             num_workers=self.num_workers,
@@ -107,8 +90,9 @@ class RecMLMDataModule(LightningDataModule):
         )
 
     def test_dataloader(self):
+        test_dataset = self.tokenize_dataset(self.test_history)
         return torch.utils.data.DataLoader(
-            RecMLMDataset(self.test_dataset),
+            RecMLMDataset(test_dataset),
             batch_size=self.batch_size,
             shuffle=False,
             num_workers=self.num_workers,

recmlm/models.py
@@ -1,11 +1,15 @@
+import argparse
 from typing import Union, List
 
 import pytorch_lightning as pl
 import torch
 from transformers import LongformerForMaskedLM, LongformerConfig
 
+from finetune import encode_all_items
+from recformer import RecformerConfig, RecformerForSeqRec, RecformerTokenizer
 
-class RecMLMConfig(LongformerConfig):
+
+class RecMLMConfig(RecformerConfig):
     def __init__(
             self,
             attention_window: Union[List[int], int] = 64,
@@ -29,12 +33,24 @@ class RecMLM(pl.LightningModule):
 
     def __init__(
             self,
+            args: argparse.Namespace,
+            config: RecMLMConfig,
             model: LongformerForMaskedLM,
-            learning_rate: float = 1e-5,):
+            tokenizer: RecformerTokenizer,
+            learning_rate: float,
+            tokenized_items: dict,
+            rec_val_dataloader: torch.utils.data.DataLoader,
+            rec_test_dataloader: torch.utils.data.DataLoader):
         super().__init__()
-
+        self.args = args
+        self.config = config
         self.model = model
         self.learning_rate = learning_rate
+
+        self.tokenizer = tokenizer
+        self.tokenized_items = tokenized_items
+        self.rec_valid_dataloader = rec_val_dataloader
+        self.rec_test_dataloader = rec_test_dataloader
 
     def forward(self, input_ids, attention_mask, global_attention_mask, label, *args, **kwargs):
         outputs = self.model(
@@ -58,6 +74,17 @@ class RecMLM(pl.LightningModule):
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
+    def on_train_epoch_end(self):
+        recformer = RecformerForSeqRec(self.config)
+        recformer.longformer = self.model.longformer
+        item_embeddings = encode_all_items(model=recformer, tokenizer=self.tokenizer,
+                                           tokenized_items=self.tokenized_items, args=self.args)
+        recformer.init_item_embedding(item_embeddings)
+        test_metrics = eval(recformer, self.rec_valid_dataloader, self.args)
+
+        self.log_dict({f"val_metric/{k}": v for k, v in test_metrics.items()},
+                      on_step=False, on_epoch=True, prog_bar=True, logger=True)
+
     def validation_step(self, batch, batch_idx):
         input_ids = batch["input_ids"]
         attention_mask = batch["attention_mask"]
@@ -76,10 +103,19 @@ class RecMLM(pl.LightningModule):
         outputs = self(input_ids, attention_mask, global_attention_mask, label)
         loss = outputs.loss
         self.log("test_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        recformer = RecformerForSeqRec(self.config)
+        recformer.longformer = self.model.longformer
+        item_embeddings = encode_all_items(recformer, self.tokenizer, self.tokenizer, self.tokenized_items)
+        recformer.init_item_embedding(item_embeddings)
+        test_metrics = eval(recformer, self.rec_test_dataloader, self.args)
+
+        self.log_dict({f"val_metric/{k}": v for k, v in test_metrics.items()},
+                      on_step=False, on_epoch=True, prog_bar=True, logger=True)
+
         return loss
 
     def configure_optimizers(self):
-
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
         return {'optimizer': optimizer, 'lr_scheduler': scheduler, 'monitor': 'val_loss'}