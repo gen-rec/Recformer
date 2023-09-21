Fix pre-training backpropagation error

recformer/models.py
@@ -64,6 +64,11 @@ class RecformerPretrainingOutput:
     global_attentions: Optional[Tuple[torch.FloatTensor]] = None
 
 
+@dataclass
+class RecformerBaseModelOutputWithPooling(LongformerBaseModelOutputWithPooling):
+    mask: Optional[torch.Tensor] = None
+
+
 def create_position_ids_from_input_ids(input_ids, padding_idx):
     """
     Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding symbols
@@ -209,40 +214,42 @@ class RecformerPooler(nn.Module):
         hidden_states: torch.Tensor,
         attr_type_ids: torch.Tensor,
         item_position_ids: torch.Tensor,
-    ) -> torch.Tensor:
+    ):
         if self.pooler_type == "attribute":
-            # attention_mask: (bs, seq_len)
-            # hidden_states: (bs, seq_len, hidden_size)
-            # attr_type_ids: (bs, seq_len)
             attr_max = attr_type_ids.max()
-
             num_items = item_position_ids.clone()
             num_items[num_items == 50] = -100
-            num_items = torch.max(num_items, dim=1).values  # (bs, )
+            num_items = torch.max(num_items, dim=1).values
             items_max = num_items.max()
 
-            # Create boolean masks for attribute and item
-            # attr_mask  (bs, attr_num, seq_len)
-            # item_mask  (bs, item_num, seq_len)
-            # attr_item_mask  (bs, attr_num, items_max, seq_len)
             attr_mask = torch.eq(
                 attr_type_ids.unsqueeze(1), torch.arange(1, attr_max + 1, device=attr_type_ids.device).reshape(1, -1, 1)
-            )  # (bs, attr_num, seq_len)
+            )
             item_mask = torch.eq(
                 item_position_ids.unsqueeze(1),
                 torch.arange(1, items_max + 1, device=item_position_ids.device).reshape(1, -1, 1),
-            )  # (bs, item_num, seq_len)
+            )
             attr_item_mask = torch.mul(attr_mask.unsqueeze(2), item_mask.unsqueeze(1))
 
-            # Select hidden_state for each item and each attribute
-            # Vectorized implementation
-            # attr_item_mask  (bs, attr_num, items_max, seq_len)  Boolean mask
-            # hidden_states  (bs, seq_len, hidden_size)
-            # hidden_states_pooled  (bs, attr_num, items_max, hidden_size)
             hidden_states_pooled = hidden_states.unsqueeze(1).unsqueeze(2) * attr_item_mask.unsqueeze(-1)
-            hidden_states_pooled[~attr_item_mask] = torch.nan
-            # (bs, attr_num, items_max, seq_len, hidden_size)
-            hidden_states_pooled = hidden_states_pooled.nanmean(dim=3)  # (bs, attr_num, items_max, hidden_size)
+
+            # Sum across the required dimension
+            summed_states = torch.sum(hidden_states_pooled, dim=3)  # Sum across the sequence length dimension
+
+            # Count the number of valid (not masked out) elements in the attr_item_mask for each position
+            valid_counts = attr_item_mask.sum(dim=3).unsqueeze(
+                -1
+            )  # Adding an extra dimension to match the dimensionality for division
+
+            valid_counts_eq_0 = torch.eq(valid_counts, 0)
+
+            # Avoid division by zero by replacing 0 counts with 1
+            valid_counts = torch.where(valid_counts_eq_0, torch.ones_like(valid_counts), valid_counts)
+
+            # Compute the mean
+            hidden_states_pooled = summed_states / valid_counts  # (bs, attr_num, items_max, hidden_size)
+
+            return hidden_states_pooled, valid_counts_eq_0.squeeze(-1)
 
         elif self.pooler_type == "item":
             num_items = item_position_ids.clone()
@@ -250,20 +257,24 @@ class RecformerPooler(nn.Module):
             num_items = torch.max(num_items, dim=1).values  # (bs, )
             items_max = num_items.max()
 
-            # item_mask  (bs, item_num, seq_len)
             item_mask = torch.eq(
                 item_position_ids.unsqueeze(1),
                 torch.arange(1, items_max + 1, device=item_position_ids.device).reshape(1, -1, 1),
             )  # (bs, item_num, seq_len)
 
-            # hidden_states  (bs, seq_len, hidden_size)
-            # hidden_states_pooled  (bs, items_max, hidden_size)
             hidden_states_pooled = hidden_states.unsqueeze(1) * item_mask.unsqueeze(
                 -1
             )  # (bs, item_num, seq_len, hidden_size)
-            hidden_states_pooled[~item_mask] = torch.nan  # (bs, items_max, seq_len, hidden_size)
-            hidden_states_pooled = hidden_states_pooled.nanmean(dim=2)  # (bs, items_max, hidden_size)
+
+            # Instead of using NaNs, use 0s and then use the mask to compute the mean
+            num_values = item_mask.sum(dim=2).unsqueeze(-1)  # Count of True values for the mean along seq_len dimension
+            sum_hidden_states = hidden_states_pooled.sum(dim=2)  # Sum along the seq_len dimension
+            hidden_states_pooled = torch.where(
+                num_values > 0, sum_hidden_states / num_values, torch.zeros_like(sum_hidden_states)
+            )
             hidden_states_pooled = hidden_states_pooled.unsqueeze(1)  # (bs, 1, items_max, hidden_size)
+
+            return hidden_states_pooled, item_mask
 
         elif self.pooler_type == "token":
             seq_len = hidden_states.shape[1]
@@ -273,14 +284,16 @@ class RecformerPooler(nn.Module):
             hidden_states_pooled[~mask] = torch.nan  # (bs, seq_len, hidden_size)
             hidden_states_pooled = hidden_states_pooled.unsqueeze(1)  # (bs, 1, seq_len, hidden_size)
 
+            return hidden_states_pooled, mask
+
         elif self.pooler_type == "cls":
             hidden_states_pooled = hidden_states[:, 0, :]  # (bs, hidden_size)
             hidden_states_pooled = hidden_states_pooled.unsqueeze(1).unsqueeze(1)  # (bs, 1, 1, hidden_size)
 
+            return hidden_states_pooled, None
+
         else:
             raise ValueError(f"pooler_type {self.pooler_type} is not supported")
-
-        return hidden_states_pooled
 
 
 class RecformerModel(LongformerPreTrainedModel):
@@ -408,7 +421,7 @@ class RecformerModel(LongformerPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, LongformerBaseModelOutputWithPooling]:
+    ) -> Union[Tuple, RecformerBaseModelOutputWithPooling]:
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
@@ -479,8 +492,8 @@ class RecformerModel(LongformerPreTrainedModel):
             return_dict=return_dict,
         )
         sequence_output = encoder_outputs[0]
-        pooled_output = (
-            self.pooler(
+        pooled_output, mask = (
+            self.pooler.forward(
                 attention_mask=attention_mask,
                 hidden_states=sequence_output,
                 attr_type_ids=attr_type_ids,
@@ -493,12 +506,13 @@ class RecformerModel(LongformerPreTrainedModel):
         if not return_dict:
             return (sequence_output, pooled_output) + encoder_outputs[1:]
 
-        return LongformerBaseModelOutputWithPooling(
+        return RecformerBaseModelOutputWithPooling(
             last_hidden_state=sequence_output,
             pooler_output=pooled_output,
             hidden_states=encoder_outputs.hidden_states,
             attentions=encoder_outputs.attentions,
             global_attentions=encoder_outputs.global_attentions,
+            mask=mask,
         )
 
 
@@ -549,10 +563,9 @@ class RecformerForPretraining(nn.Module):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
     ):
-
         batch_size = input_ids_a.size(0)
 
-        outputs_a = self.longformer(
+        outputs_a = self.longformer.forward(
             input_ids_a,
             attention_mask=attention_mask_a,
             global_attention_mask=global_attention_mask_a,
@@ -566,7 +579,7 @@ class RecformerForPretraining(nn.Module):
             output_hidden_states=output_hidden_states,
             return_dict=True,
         )
-        outputs_b = self.longformer(
+        outputs_b = self.longformer.forward(
             input_ids_b,
             attention_mask=attention_mask_b,
             global_attention_mask=global_attention_mask_b,
@@ -584,7 +597,7 @@ class RecformerForPretraining(nn.Module):
         # MLM auxiliary objective
         mlm_outputs_a = None
         if mlm_input_ids_a is not None:
-            mlm_outputs_a = self.longformer(
+            mlm_outputs_a = self.longformer.forward(
                 mlm_input_ids_a,
                 attention_mask=attention_mask_a,
                 global_attention_mask=global_attention_mask_a,
@@ -601,7 +614,7 @@ class RecformerForPretraining(nn.Module):
 
         mlm_outputs_b = None
         if mlm_input_ids_b is not None:
-            mlm_outputs_b = self.longformer(
+            mlm_outputs_b = self.longformer.forward(
                 mlm_input_ids_b,
                 attention_mask=attention_mask_b,
                 global_attention_mask=global_attention_mask_b,
@@ -616,35 +629,70 @@ class RecformerForPretraining(nn.Module):
                 return_dict=True,
             )
 
-        z1 = outputs_a.pooler_output  # (bs*num_sent, hidden_size)
-        z2 = outputs_b.pooler_output  # (bs*num_sent, hidden_size)
+        z1 = outputs_a.pooler_output  # (bs, attr_num, session_max, hidden_size)
+        z2 = outputs_b.pooler_output  # (bs, attr_num, 1, hidden_size)
+
+        z1_mask = outputs_a.mask  # (bs, attr_num, session_max)
+        z2_mask = outputs_b.mask  # (bs, attr_num, 1)
 
         # Gather all embeddings if using distributed training
         if self.training and distributed.is_initialized():
+            # Pad z1 to (bs, attr_num, max_item_embeddings, hidden_size)
+            z1_padded = torch.empty(
+                (batch_size, z1.size(1), self.config.max_item_embeddings, z1.size(3)), device=z1.device
+            )
+            z1_padded[:, :, : z1.size(2), :] = z1
+
             # Dummy vectors for allgather
-            z1_list = [torch.zeros_like(z1) for _ in range(distributed.get_world_size())]
-            z2_list = [torch.zeros_like(z2) for _ in range(distributed.get_world_size())]
+            z1_list = [torch.empty_like(z1_padded) for _ in range(distributed.get_world_size())]
+            z2_list = [torch.empty_like(z2) for _ in range(distributed.get_world_size())]
 
             # Allgather
-            distributed.all_gather(tensor_list=z1_list, tensor=z1.contiguous())
+            distributed.all_gather(tensor_list=z1_list, tensor=z1_padded.contiguous())
             distributed.all_gather(tensor_list=z2_list, tensor=z2.contiguous())
 
             # Since allgather results do not have gradients, we replace the
             # current process's corresponding embeddings with original tensors
-            z1_list[distributed.get_rank()] = z1
+            z1_list[distributed.get_rank()] = z1_padded
             z2_list[distributed.get_rank()] = z2
 
             # Get full batch embeddings: (bs x N, hidden)
             z1 = torch.cat(z1_list, 0)
             z2 = torch.cat(z2_list, 0)
 
+            if z1_mask is not None and z2_mask is not None:
+                z1_mask_padded = torch.ones(
+                    (batch_size, z1_mask.size(1), self.config.max_item_embeddings), dtype=torch.bool, device=z1_mask.device
+                )
+                z1_mask_padded[:, :, : z1_mask.size(2)] = z1_mask
+
+                z1_mask_list = [torch.empty_like(z1_mask_padded) for _ in range(distributed.get_world_size())]
+                z2_mask_list = [torch.empty_like(z2_mask) for _ in range(distributed.get_world_size())]
+
+                distributed.all_gather(tensor_list=z1_mask_list, tensor=z1_mask_padded.contiguous())
+                distributed.all_gather(tensor_list=z2_mask_list, tensor=z2_mask.contiguous())
+
+                z1_mask = torch.cat(z1_mask_list, 0)
+                z2_mask = torch.cat(z2_mask_list, 0)
+
         # z1: (bs, attr_num, items_max, hidden_size)
         # z2: (bs, attr_num, 1, hidden_size)
-        scores = self.sim(z1.unsqueeze(1), z2)  # (bs, bs, attr_num, items_max)
+        # z1_mask: (bs, attr_num, items_max)
+        # z2_mask: (bs, attr_num, 1)
+        z1.unsqueeze_(1)  # (bs, 1, attr_num, items_max, hidden_size)
+        z2.unsqueeze_(0)  # (1, bs, attr_num, 1, hidden_size)
+        z1_mask.unsqueeze_(1)  # (bs, 1, attr_num, items_max)
+        z2_mask.unsqueeze_(0)  # (1, bs, attr_num, 1)
+
+        scores = self.sim.forward(z1, z2)  # (bs, bs, attr_num, items_max)
+
+        mask = torch.add(z1_mask, z2_mask)  # (bs, bs, attr_num, items_max)
+        scores[mask] = -torch.inf
+
         cos_sim = reduce_session(scores, self.config.session_reduce_method, self.config.session_reduce_topk)  # (bs, bs)
         labels = torch.arange(cos_sim.size(0), device=cos_sim.device)
 
-        loss = cross_entropy(cos_sim, labels)
+        ce_loss = cross_entropy(cos_sim, labels)
         correct_num = torch.eq(torch.argmax(cos_sim, 1), labels).sum()
 
         if mlm_outputs_a is not None and mlm_labels_a is not None:
@@ -653,7 +701,9 @@ class RecformerForPretraining(nn.Module):
             masked_lm_loss_a = cross_entropy(
                 prediction_scores_a.view(-1, self.config.vocab_size), mlm_labels_a.view(-1)
             )
-            loss = loss + self.config.mlm_weight * masked_lm_loss_a
+            mlm_loss_a = self.config.mlm_weight * masked_lm_loss_a
+        else:
+            mlm_loss_a = 0
 
         if mlm_outputs_b is not None and mlm_labels_b is not None:
             mlm_labels_b = mlm_labels_b.view(-1, mlm_labels_b.size(-1))
@@ -661,7 +711,11 @@ class RecformerForPretraining(nn.Module):
             masked_lm_loss_b = cross_entropy(
                 prediction_scores_b.view(-1, self.config.vocab_size), mlm_labels_b.view(-1)
             )
-            loss = loss + self.config.mlm_weight * masked_lm_loss_b
+            mlm_loss_b = self.config.mlm_weight * masked_lm_loss_b
+        else:
+            mlm_loss_b = 0
+
+        loss = ce_loss + mlm_loss_a + mlm_loss_b
 
         return RecformerPretrainingOutput(
             loss=loss,
@@ -694,7 +748,7 @@ class RecformerForSeqRec(LongformerPreTrainedModel):
         if candidates is not None:
             raise NotImplementedError("Negative sampling disabled")
 
-        candidate_embeddings = self.item_embedding
+        candidate_embeddings = self.item_embedding  # (|I|, attr_num, 1, hidden_size)
         pooler_output = pooler_output.unsqueeze(1)  # (batch_size, 1, attr_num, items_max, hidden_size)
         sim = self.sim(pooler_output, candidate_embeddings)  # (batch_size, |I|, attr_num, items_max)
 
@@ -775,21 +829,27 @@ def reduce_session(
     session_reduce_topk: int | None = None,
     session_reduce_weightedsim_temp: float = 1.0,
 ):
+    """
+    Mask tensor: True to mask
+    """
     scores: torch.Tensor  # (bs, |I|, attr_num, items_max)
+    mask_tensor: torch.Tensor  # (bs, |I|, attr_num, items_max)  # True for valid tokens
 
     if session_reduce_method == "maxsim":
         # Replace NaN with -inf
-        scores[torch.isnan(scores)] = -torch.inf  # (bs, |I|, attr_num, items_max)
         scores = scores.max(dim=-1).values  # (bs, |I|, num_attr)
     elif session_reduce_method == "mean":
+        raise NotImplementedError("Mean pooling disabled")
         scores = scores.nanmean(dim=-1)  # (bs, |I|, num_attr)
     elif session_reduce_method == "weightedsim":
+        raise NotImplementedError("Weighted pooling disabled")
         scores[torch.isnan(scores)] = -torch.inf
         scores = scores / session_reduce_weightedsim_temp  # (bs, |I|, num_attr, items_max)
         weights = torch.softmax(scores, dim=-1)  # (bs, |I|, num_attr, items_max)
         scores = torch.mul(scores, weights)  # (bs, |I|, num_attr, items_max)
         scores = scores.nanmean(dim=-1)  # (bs, |I|, num_attr)
     elif session_reduce_method == "topksim":
+        raise NotImplementedError("Topk pooling disabled")
         session_reduce_topk = min(session_reduce_topk, scores.shape[-1])
         scores[torch.isnan(scores)] = -torch.inf
         scores = scores.topk(session_reduce_topk, dim=-1).values  # (bs, |I|, num_attr, topk)