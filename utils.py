289058200611e52b66f6f0a8c0b3c8a38b66562c
sudokim <khs1778@g.skku.edu>
2023-10-03 00:18:29 +0900

    Add option to set seed

--- utils.py
+++ utils.py
@@ -40,6 +40,7 @@ def load_data(args):
 def parse_finetune_args():
     parser = ArgumentParser()
     parser.add_argument("--server", type=str, required=True)
+    parser.add_argument("--seed", type=int, default=42)
     # path and file
     parser.add_argument("--pretrain_ckpt", type=str, default=None, required=True)
     parser.add_argument("--data_path", type=Path, default=None, required=True)
