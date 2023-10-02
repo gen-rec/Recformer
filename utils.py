400ddc704fc61d1128b6e58cab4f70bc930c81b3
sudokim <khs1778@g.skku.edu>
2023-10-03 01:07:22 +0900

    Add option to change group name

--- utils.py
+++ utils.py
@@ -42,6 +42,7 @@ def parse_finetune_args():
     parser.add_argument("--server", type=str, required=True)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--linear_out", type=int, default=256)
+    parser.add_argument("--group_name", type=str, default=None)
     # path and file
     parser.add_argument("--pretrain_ckpt", type=str, default=None, required=True)
     parser.add_argument("--data_path", type=Path, default=None, required=True)
