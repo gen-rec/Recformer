44e3a22e7826109819c599a7b256910f4d5b6f2e
junieberry <junyoung44@g.skku.edu>
2023-10-10 06:02:21 +0900

    fix analysis code

--- analysis/diversity.py
+++ analysis/diversity.py
@@ -28,39 +28,30 @@ def shannon_entropy(item_probability):
     return -sum([p * np.log(p) for p in item_probability])
 
 
-def diversity(model_name, data_name):
-    prediction_path = Path('../prediction') / Path(f"{model_name}") / Path(f"{data_name}") / Path(
-        'predictions.json')
-    prediction = json.load(open(prediction_path, 'r'))
-
+def diversity(prediction):
     diversity_result = dict()
     print("\n\n")
     for cutoff in [10, 20, 50]:
-        print("="*20)
-        print(f"{model_name} {data_name} cutoff: {cutoff}")
+        print("=" * 20)
+        print(f"cutoff: {cutoff}")
         item_counter = Counter()
-        for item in prediction['predictions']:
-            item_counter.update(item[:cutoff])
+        for user_id, user_prediction in prediction.items():
+            item_counter.update(user_prediction['predictions'][:cutoff])
 
         item_probability = [v / sum(item_counter.values()) for v in item_counter.values()]
 
         gini_index = gini(item_probability)
         entropy = shannon_entropy(item_probability)
 
-        print(f"{model_name} {data_name} gini: {gini_index}")
-        print(f"{model_name} {data_name} entropy: {entropy}")
+        print(f"gini: {gini_index}")
+        print(f"entropy: {entropy}")
 
         diversity_result[cutoff] = {"gini": gini_index, "shannon_entropy": entropy}
 
-    json.dump(diversity_result,
-              open(Path('../prediction') / Path(f"{model_name}") / Path(f"{data_name}") / Path('diversity.json'), 'w'))
-
 
 if __name__ == "__main__":
-
-    model_list = ["Recformer", "MIRACLE"]
-    data = ["Scientific", "Instruments", "Pantry"]
-
-    for model in model_list:
-        for d in data:
-            diversity(model, d)
+    file_name = "MARS_Pantry_1398.json"
+    prediction_path = Path('../prediction') / Path(file_name)
+    prediction = json.load(open(prediction_path, 'r'))
+    print(f"prediction: {prediction_path}")
+    diversity(prediction)
