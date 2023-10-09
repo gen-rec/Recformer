5b25c6efee4a6e1a949d06eea0230f887ae2180d
junieberry <junyoung44@g.skku.edu>
2023-10-10 06:02:21 +0900

    fix analysis code

--- analysis/popularity_bias.py
+++ analysis/popularity_bias.py
@@ -5,7 +5,14 @@ from typing import List
 import json
 from pathlib import Path
 
+from finetune import load_data
+
 Item = int
+
+
+class Namespace():
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
 
 def parse_args():
@@ -41,37 +48,38 @@ def ndcg(predictions: List[List[Item]], targets: List[Item], k: int = 10) -> flo
     return sum(scores) / len(scores)
 
 
-def popularity_bias(model_name, data_name):
+def popularity_bias(data_name, prediction):
     item_split = json.load(open(Path('../finetune_data') / Path(f"{data_name}_ours") / Path('item_split.json'), 'r'))
     user_split = json.load(open(Path('../finetune_data') / Path(f"{data_name}_ours") / Path('user_split.json'), 'r'))
-    prediction_path = Path('../prediction') / Path(f"{model_name}") / Path(f"{data_name}") / Path(
-        'predictions.json')
-    prediction = json.load(open(prediction_path, 'r'))
 
+    overall_prediction, overall_target = [], []
     head_item_prediction, head_target = [], []
     tail_item_prediction, tail_target = [], []
     head_user_prediction, head_user_target = [], []
     tail_user_prediction, tail_user_target = [], []
 
-    for p, t, u in zip(prediction['predictions'], prediction['targets'], prediction['users']):
-        if t in item_split['head']:
-            head_item_prediction.append(p)
-            head_target.append(t)
-        else:
-            tail_item_prediction.append(p)
-            tail_target.append(t)
+    for user, user_prediction in prediction.items():
+        overall_prediction.append(user_prediction['predictions'])
+        overall_target.append(user_prediction['target'])
 
-        if u in user_split['head']:
-            head_user_prediction.append(p)
-            head_user_target.append(t)
+        if int(user) in user_split['head']:
+            head_user_prediction.append(user_prediction['predictions'])
+            head_user_target.append(user_prediction['target'])
         else:
-            tail_user_prediction.append(p)
-            tail_user_target.append(t)
+            tail_user_prediction.append(user_prediction['predictions'])
+            tail_user_target.append(user_prediction['target'])
+
+        if int(user_prediction['target']) in item_split['head']:
+            head_item_prediction.append(user_prediction['predictions'])
+            head_target.append(user_prediction['target'])
+        else:
+            tail_item_prediction.append(user_prediction['predictions'])
+            tail_target.append(user_prediction['target'])
 
     bias = {
         'overall': {
-            'recall@10': recall(prediction['predictions'], prediction['targets'], 10),
-            'ndcg@10': ndcg(prediction['predictions'], prediction['targets'], 10),
+            'recall@10': recall(overall_prediction, overall_target, 10),
+            'ndcg@10': ndcg(overall_prediction, overall_target, 10),
         },
         'head_item': {
             'recall@10': recall(head_item_prediction, head_target, 10),
@@ -109,10 +117,9 @@ def popularity_bias(model_name, data_name):
 
     print("\n\n\n\n")
     print("=" * 50)
-    print(f"model: {model_name}, data: {data_name}")
 
-    print(f"percentage of head item: {len(head_item_prediction) / len(prediction['predictions'])}")
-    print(f"percentage of head user: {len(head_user_prediction) / len(prediction['predictions'])}")
+    print(f"percentage of head item: {len(head_item_prediction) / len(overall_prediction)}")
+    print(f"percentage of head user: {len(head_user_prediction) / len(overall_prediction)}")
 
     print(f"overall | recall@10: {bias['overall']['recall@10']:.4f} , ndcg@10: {bias['overall']['ndcg@10']:.4f}")
     print(f"head item | recall@10: {bias['head_item']['recall@10']:.4f} , ndcg@10: {bias['head_item']['ndcg@10']:.4f}")
@@ -120,16 +127,32 @@ def popularity_bias(model_name, data_name):
     print(f"head user | recall@10: {bias['head_user']['recall@10']:.4f} , ndcg@10: {bias['head_user']['ndcg@10']:.4f}")
     print(f"tail user | recall@10: {bias['tail_user']['recall@10']:.4f} , ndcg@10: {bias['tail_user']['ndcg@10']:.4f}")
 
-    json.dump(bias,
-              open(Path('../prediction') / Path(f"{model_name}") / Path(f"{data_name}") / Path('bias.json'),
-                   'w'), indent=1, ensure_ascii=False)
+
+def asin2id(file, data):
+    args = Namespace(
+        data_path= data,
+        train_file="train.json",
+        dev_file="val.json",
+        test_file="test.json",
+        item2id_file="smap.json",
+        meta_file="meta_data.json",
+    )
+    train, val, test, item_meta_dict, item2id, id2item = load_data(args)
+    user2id = json.load(open(Path(data) / Path('umap.json'), 'r'))
+    new_prediction = dict()
+    for user, user_prediction in file.items():
+        new_prediction[user2id[user]] = {
+            'predictions': [item2id[item] for item in user_prediction['predictions']],
+            'target': item2id[user_prediction['target']]
+        }
+    return new_prediction
 
 
 if __name__ == "__main__":
 
-    model_list = ["BERT4Rec", "FDSA", "SASRec", "UniSRec"]
-    data = ["Scientific", "Instruments"]
-
-    for model in model_list:
-        for d in data:
-            popularity_bias(model, d)
+    file_name = "SASRec_Instruments_42.json"
+    model, data, _ = file_name.split('_')
+    prediction = json.load(open(Path('../prediction') / Path(file_name), 'r'))
+    if model == 'MARS':
+        prediction = asin2id(prediction, f"../finetune_data/{data}_ours")
+    popularity_bias(data, prediction)
