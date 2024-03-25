80cce8ce311a795cad32f8a9f7c2bdd9c1dce803
junieberry <junyoung44@g.skku.edu>
2024-03-25 14:31:30 +0900

    refactor some code

--- finetune.py
+++ finetune.py
@@ -1,8 +1,13 @@
 import json
 import os
 from argparse import ArgumentParser
+from datetime import datetime
 from multiprocessing import Pool
 from pathlib import Path
+
+from wonderwords import RandomWord
+
+import wandb
 
 import torch
 from pytorch_lightning import seed_everything
@@ -14,32 +19,13 @@ from collator import FinetuneDataCollatorWithPadding, EvalDataCollatorWithPaddin
 from dataloader import RecformerTrainDataset, RecformerEvalDataset
 from optimization import create_optimizer_and_scheduler
 from recformer import RecformerModel, RecformerForSeqRec, RecformerTokenizer, RecformerConfig
-from utils import read_json, AverageMeterSet, Ranker
+from utils import read_json, AverageMeterSet, Ranker, parse_args, load_data
 
-
-def load_data(args):
-
-    train = read_json(os.path.join(args.data_path, args.train_file), True)
-    val = read_json(os.path.join(args.data_path, args.dev_file), True)
-    test = read_json(os.path.join(args.data_path, args.test_file), True)
-    item_meta_dict = json.load(open(os.path.join(args.data_path, args.meta_file)))
-
-    item2id = read_json(os.path.join(args.data_path, args.item2id_file))
-    id2item = {v: k for k, v in item2id.items()}
-
-    item_meta_dict_filted = dict()
-    for k, v in item_meta_dict.items():
-        if k in item2id:
-            item_meta_dict_filted[k] = v
-
-    return train, val, test, item_meta_dict_filted, item2id, id2item
-
-
+wandb_logger: wandb.sdk.wandb_run.Run | None = None
 tokenizer_glb: RecformerTokenizer = None
 
 
 def _par_tokenize_doc(doc):
-
     item_id, item_attr = doc
 
     input_ids, token_type_ids = tokenizer_glb.encode_item(item_attr)
@@ -48,7 +34,6 @@ def _par_tokenize_doc(doc):
 
 
 def encode_all_items(model: RecformerModel, tokenizer: RecformerTokenizer, tokenized_items, args):
-
     model.eval()
 
     items = sorted(list(tokenized_items.items()), key=lambda x: x[0])
@@ -59,7 +44,7 @@ def encode_all_items(model: RecformerModel, tokenizer: RecformerTokenizer, token
     with torch.no_grad():
         for i in tqdm(range(0, len(items), args.batch_size), ncols=100, desc="Encode all items"):
 
-            item_batch = [[item] for item in items[i : i + args.batch_size]]
+            item_batch = [[item] for item in items[i: i + args.batch_size]]
 
             inputs = tokenizer.batch_encode(item_batch, encode_item=False)
 
@@ -75,8 +60,7 @@ def encode_all_items(model: RecformerModel, tokenizer: RecformerTokenizer, token
     return item_embeddings
 
 
-def eval(model, dataloader, args):
-
+def evaluate(model, dataloader, args):
     model.eval()
 
     ranker = Ranker(args.metric_ks)
@@ -108,7 +92,10 @@ def eval(model, dataloader, args):
     return average_metrics
 
 
-def train_one_epoch(model, dataloader, optimizer, scheduler, scaler, args):
+def train_one_epoch(model, dataloader, optimizer, scheduler, args, train_step: int):
+    global wandb_logger
+
+    epoch_losses = []
 
     model.train()
 
@@ -116,126 +103,80 @@ def train_one_epoch(model, dataloader, optimizer, scheduler, scaler, args):
         for k, v in batch.items():
             batch[k] = v.to(args.device)
 
-        if args.fp16:
-            with autocast():
-                loss = model(**batch)
-        else:
+        with autocast(dtype=torch.bfloat16, enabled=args.bf16):
             loss = model(**batch)
+
+        if torch.any(torch.isnan(loss)):
+            continue
+
+        if wandb_logger is not None:
+            wandb_logger.log({f"train_step_{train_step}/loss": loss.item()})
+            epoch_losses.append(loss.item())
 
         if args.gradient_accumulation_steps > 1:
             loss = loss / args.gradient_accumulation_steps
 
-        if args.fp16:
-            scaler.scale(loss).backward()
-        else:
-            loss.backward()
+        loss.backward()
 
-        if (step + 1) % args.gradient_accumulation_steps == 0:
-            if args.fp16:
+        if (step + 1) % args.gradient_accumulation_steps == 0 or step == len(dataloader) - 1:
+            optimizer.step()
+            optimizer.zero_grad()
+            scheduler.step()  # Update learning rate schedule
 
-                scale_before = scaler.get_scale()
-                scaler.step(optimizer)
-                scaler.update()
-                scale_after = scaler.get_scale()
-                optimizer_was_run = scale_before <= scale_after
-                optimizer.zero_grad()
-
-                if optimizer_was_run:
-                    scheduler.step()
-
-            else:
-
-                scheduler.step()  # Update learning rate schedule
-                optimizer.step()
-                optimizer.zero_grad()
+    if wandb_logger is not None:
+        wandb_logger.log({f"train_step_{train_step}/epoch_loss": sum(epoch_losses) / len(epoch_losses)})
 
 
-def main():
-    parser = ArgumentParser()
-    # path and file
-    parser.add_argument("--pretrain_ckpt", type=str, default=None, required=True)
-    parser.add_argument("--data_path", type=str, default=None, required=True)
-    parser.add_argument("--output_dir", type=str, default="checkpoints")
-    parser.add_argument("--ckpt", type=str, default="best_model.bin")
-    parser.add_argument("--model_name_or_path", type=str, default="allenai/longformer-base-4096")
-    parser.add_argument("--train_file", type=str, default="train.json")
-    parser.add_argument("--dev_file", type=str, default="val.json")
-    parser.add_argument("--test_file", type=str, default="test.json")
-    parser.add_argument("--item2id_file", type=str, default="smap.json")
-    parser.add_argument("--meta_file", type=str, default="meta_data.json")
-
-    # data process
-    parser.add_argument(
-        "--preprocessing_num_workers", type=int, default=8, help="The number of processes to use for the preprocessing."
-    )
-    parser.add_argument("--dataloader_num_workers", type=int, default=0)
-
-    # model
-    parser.add_argument("--max_token_num", type=int, default=1024, help="Max token number for longformer.")
-    parser.add_argument("--temp", type=float, default=0.05, help="Temperature for softmax.")
-
-    # train
-    parser.add_argument("--num_train_epochs", type=int, default=16)
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
-    parser.add_argument("--finetune_negative_sample_size", type=int, default=1000)
-    parser.add_argument("--metric_ks", nargs="+", type=int, default=[10, 50], help="ks for Metric@k")
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--learning_rate", type=float, default=5e-5)
-    parser.add_argument("--weight_decay", type=float, default=0)
-    parser.add_argument("--warmup_steps", type=int, default=100)
-    parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--fp16", action="store_true")
-    parser.add_argument("--fix_word_embedding", action="store_true")
-    parser.add_argument("--verbose", type=int, default=3)
-
-    args = parser.parse_args()
+def main(args):
     print(args)
     seed_everything(42)
     args.device = torch.device("cuda:{}".format(args.device)) if args.device >= 0 else torch.device("cpu")
 
-    train, val, test, item_meta_dict, item2id, id2item = load_data(args)
-
-    config = RecformerConfig.from_pretrained(args.model_name_or_path)
-    config.max_attr_num = 3
-    config.max_attr_length = 32
-    config.max_item_embeddings = 51
-    config.attention_window = [64] * 12
-    config.max_token_num = args.max_token_num
-    config.item_num = len(item2id)
-    config.finetune_negative_sample_size = args.finetune_negative_sample_size
-    tokenizer = RecformerTokenizer.from_pretrained(args.model_name_or_path, config)
+    train, val, test, item_meta_dict, item2id, id2item, id2user = load_data(args)
+    config, tokenizer = load_config_tokenizer(args, item2id)
 
     global tokenizer_glb
     tokenizer_glb = tokenizer
 
+    random_word_generator = RandomWord()
+    while True:
+        random_word = random_word_generator.random_words(include_parts_of_speech=["noun", "verb"])[0]
+        if " " in random_word or "-" in random_word:
+            continue
+        else:
+            break
+    server_random_word_and_date = args.server + "_" + random_word + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
     path_corpus = Path(args.data_path)
-    dir_preprocess = path_corpus / "preprocess"
-    dir_preprocess.mkdir(exist_ok=True)
+    path_output = Path(args.output_dir) / random_word
 
-    path_output = Path(args.output_dir) / path_corpus.name
-    path_output.mkdir(exist_ok=True, parents=True)
-    path_ckpt = path_output / args.ckpt
+    try:
+        path_output.mkdir(exist_ok=False, parents=True)
+    except FileExistsError:
+        raise FileExistsError(f"Output directory ({path_output}) already exists.")
 
-    path_tokenized_items = dir_preprocess / f"tokenized_items_{path_corpus.name}"
+    global wandb_logger
+    wandb_logger = wandb.init(
+        project="AfterSIGIR",
+        entity="gen-rec",
+        name=server_random_word_and_date,
+        group=args.group_name or path_corpus.name,
+        config=vars(args),
+        tags=[
+            path_corpus.name,
+            f"seed_{args.seed}",
+        ],
+    )
 
-    if path_tokenized_items.exists():
-        print(f"[Preprocessor] Use cache: {path_tokenized_items}")
-    else:
-        print(f"Loading attribute data {path_corpus}")
-        pool = Pool(processes=args.preprocessing_num_workers)
-        pool_func = pool.imap(func=_par_tokenize_doc, iterable=item_meta_dict.items())
-        doc_tuples = list(tqdm(pool_func, total=len(item_meta_dict), ncols=100, desc=f"[Tokenize] {path_corpus}"))
-        tokenized_items = {
-            item2id[item_id]: [input_ids, token_type_ids] for item_id, input_ids, token_type_ids in doc_tuples
-        }
-        pool.close()
-        pool.join()
+    doc_tuples = [
+        _par_tokenize_doc(doc) for doc in tqdm(item_meta_dict.items(), ncols=100, desc=f"[Tokenize] {path_corpus}")
+    ]
+    tokenized_items = {
+        item2id[item_id]: [input_ids, token_type_ids, attr_type_ids]
+        for item_id, input_ids, token_type_ids, attr_type_ids in doc_tuples
+    }
 
-        torch.save(tokenized_items, path_tokenized_items)
-
-    tokenized_items = torch.load(path_tokenized_items)
     print(f"Successfully load {len(tokenized_items)} tokenized items.")
-
     finetune_data_collator = FinetuneDataCollatorWithPadding(tokenizer, tokenized_items)
     eval_data_collator = EvalDataCollatorWithPadding(tokenizer, tokenized_items)
 
@@ -257,15 +198,8 @@ def main():
         for param in model.longformer.embeddings.word_embeddings.parameters():
             param.requires_grad = False
 
-    path_item_embeddings = dir_preprocess / f"item_embeddings_{path_corpus.name}"
-    if path_item_embeddings.exists():
-        print(f"[Item Embeddings] Use cache: {path_tokenized_items}")
-    else:
-        print(f"Encoding items.")
-        item_embeddings = encode_all_items(model.longformer, tokenizer, tokenized_items, args)
-        torch.save(item_embeddings, path_item_embeddings)
-
-    item_embeddings = torch.load(path_item_embeddings)
+    print(f"Encoding items.")
+    item_embeddings = encode_all_items(model.longformer, tokenizer, tokenized_items, args)
     model.init_item_embedding(item_embeddings)
 
     model.to(args.device)  # send item embeddings to device
@@ -278,7 +212,7 @@ def main():
     else:
         scaler = None
 
-    test_metrics = eval(model, test_loader, args)
+    test_metrics = evaluate(model, test_loader, args)
     print(f"Test set: {test_metrics}")
 
     best_target = float("-inf")
@@ -292,14 +226,14 @@ def main():
         train_one_epoch(model, train_loader, optimizer, scheduler, scaler, args)
 
         if (epoch + 1) % args.verbose == 0:
-            dev_metrics = eval(model, dev_loader, args)
+            dev_metrics = evaluate(model, dev_loader, args)
             print(f"Epoch: {epoch}. Dev set: {dev_metrics}")
 
             if dev_metrics["NDCG@10"] > best_target:
                 print("Save the best model.")
                 best_target = dev_metrics["NDCG@10"]
                 patient = 5
-                torch.save(model.state_dict(), path_ckpt)
+                torch.save(model.state_dict(), path_output / "stage_1_best_model.pt")
 
             else:
                 patient -= 1
@@ -307,7 +241,12 @@ def main():
                     break
 
     print("Load best model in stage 1.")
-    model.load_state_dict(torch.load(path_ckpt))
+    model.load_state_dict(torch.load(path_output / "stage_1_best_model.pt"))
+
+    test_metrics = evaluate(model, test_loader, args)
+    print(f"Stage-1 Test set: {test_metrics}")
+    if wandb_logger is not None:
+        wandb_logger.log({f"stage_1_test/{k}": v for k, v in test_metrics.items()})
 
     patient = 3
 
@@ -316,25 +255,59 @@ def main():
         train_one_epoch(model, train_loader, optimizer, scheduler, scaler, args)
 
         if (epoch + 1) % args.verbose == 0:
-            dev_metrics = eval(model, dev_loader, args)
+            dev_metrics = evaluate(model, dev_loader, args)
             print(f"Epoch: {epoch}. Dev set: {dev_metrics}")
 
             if dev_metrics["NDCG@10"] > best_target:
                 print("Save the best model.")
                 best_target = dev_metrics["NDCG@10"]
                 patient = 3
-                torch.save(model.state_dict(), path_ckpt)
+                torch.save(model.state_dict(), path_output / "stage_2_best_model.pt")
 
             else:
                 patient -= 1
                 if patient == 0:
                     break
 
-    print("Test with the best checkpoint.")
-    model.load_state_dict(torch.load(path_ckpt))
-    test_metrics = eval(model, test_loader, args)
-    print(f"Test set: {test_metrics}")
+    print("Load best model in stage 2.")
+    try:
+        model.load_state_dict(torch.load(path_output / "stage_2_best.pt"))
+    except FileNotFoundError:
+        print("No best model in stage 2. Use the latest model.")
+
+    test_metrics, predictions, labels = evaluate(model, test_loader, args, return_preds=True)
+    print(f"Stage-2 Test set: {test_metrics}")
+
+    if wandb_logger is not None:
+        wandb_logger.log({f"stage_2_test/{k}": v for k, v in test_metrics.items()})
+
+    users = list(map(int, test.keys()))
+    users = list(map(id2user.get, users))
+
+    predictions = predictions.tolist()
+    labels = labels.tolist()
+
+    output = {}
+    for user, prediction, label in zip(users, predictions, labels):
+        prediction = list(map(id2item.get, prediction))
+        label = id2item[label]
+        output[user] = {"predictions": prediction, "target": label}
+
+    json.dump(output, open(path_output / "predictions.json", "w"), indent=1, ensure_ascii=False)
+
+def load_config_tokenizer(args, item2id):
+    config = RecformerConfig.from_pretrained(args.model_name_or_path)
+    config.max_attr_num = 3
+    config.max_attr_length = 32
+    config.max_item_embeddings = 51
+    config.attention_window = [64] * 12
+    config.max_token_num = args.max_token_num
+    config.item_num = len(item2id)
+    config.finetune_negative_sample_size = args.finetune_negative_sample_size
+    tokenizer = RecformerTokenizer.from_pretrained(args.model_name_or_path, config)
+    return config, tokenizer
 
 
 if __name__ == "__main__":
-    main()
+    arg = parse_args()
+    main(arg)
--- utils.py
+++ utils.py
@@ -1,9 +1,76 @@
 import json
+from argparse import ArgumentParser
+import os
 
 import torch
 import torch.nn as nn
 
 MAX_VAL = 1e4
+
+
+def parse_args():
+    parser = ArgumentParser()
+    # path and file
+    parser.add_argument("--server", type=int, required=True)
+    parser.add_argument("--pretrain_ckpt", type=str, default=None, required=True)
+    parser.add_argument("--data_path", type=str, default=None, required=True)
+    parser.add_argument("--output_dir", type=str, default="checkpoints")
+    parser.add_argument("--ckpt", type=str, default="best_model.bin")
+    parser.add_argument("--model_name_or_path", type=str, default="allenai/longformer-base-4096")
+    parser.add_argument("--train_file", type=str, default="train.json")
+    parser.add_argument("--dev_file", type=str, default="val.json")
+    parser.add_argument("--test_file", type=str, default="test.json")
+    parser.add_argument("--item2id_file", type=str, default="smap.json")
+    parser.add_argument("--meta_file", type=str, default="meta_data.json")
+
+    # data process
+    parser.add_argument(
+        "--preprocessing_num_workers", type=int, default=8, help="The number of processes to use for the preprocessing."
+    )
+    parser.add_argument("--dataloader_num_workers", type=int, default=0)
+
+    # model
+    parser.add_argument("--max_token_num", type=int, default=1024, help="Max token number for longformer.")
+    parser.add_argument("--temp", type=float, default=0.05, help="Temperature for softmax.")
+
+    # wandb
+    parser.add_argument("--wandb_group", type=str)
+
+    # train
+    parser.add_argument("--num_train_epochs", type=int, default=16)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
+    parser.add_argument("--finetune_negative_sample_size", type=int, default=1000)
+    parser.add_argument("--metric_ks", nargs="+", type=int, default=[10, 50], help="ks for Metric@k")
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--weight_decay", type=float, default=0)
+    parser.add_argument("--warmup_steps", type=int, default=100)
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--fp16", action="store_true")
+    parser.add_argument("--fix_word_embedding", action="store_true")
+    parser.add_argument("--verbose", type=int, default=3)
+    parser.add_argument("--seed", type=int, default=42)
+
+    args = parser.parse_args()
+    return args
+
+
+def load_data(args):
+    train = read_json(os.path.join(args.data_path, args.train_file), True)
+    val = read_json(os.path.join(args.data_path, args.dev_file), True)
+    test = read_json(os.path.join(args.data_path, args.test_file), True)
+    item_meta_dict = json.load(open(os.path.join(args.data_path, args.meta_file)))
+
+    item2id = read_json(os.path.join(args.data_path, args.item2id_file))
+    id2item = {v: k for k, v in item2id.items()}
+
+    item_meta_dict_filted = dict()
+    for k, v in item_meta_dict.items():
+        if k in item2id:
+            item_meta_dict_filted[k] = v
+    id2user = {v: k for k, v in item2id.items()}
+
+    return train, val, test, item_meta_dict_filted, item2id, id2item, id2user
 
 
 def read_json(path, as_int=False):
